4c246fcf7667e858d3826407df849c7990840d02
Thorben Groos <thorben.groos@student.uni-siegen.de>
2020-12-03 17:05:21 +0100

    Imprived algorithm memory usage

--- RC4_JS_geringe_GUI_Interaktion/RC4.js
+++ RC4_JS_geringe_GUI_Interaktion/RC4.js
@@ -13,34 +13,37 @@ function rc4(key, plaintext) {
 	var x;
 	var keylength = key.length;
 	var strlength = plaintext.length;
-	
+
 	for (var i = 0; i < 256; i++) {
 		s[i] = i;
 	}
-	
+
 	for (i = 0; i < 256; i++) {
 		j = (j + s[i] + key[i % keylength]) % 256;
 		x = s[i];
 		s[i] = s[j];
 		s[j] = x;
 	}
-	
+
 	i = 0;
 	j = 0;
 	ciphertext = [];
-	vector = [];
-	var temp;
-	
+	//ciphertext = new Uint32Array(plaintext);
+	//vector = [];
+	//var temp;
+
 	for (var y = 0; y < strlength; y++) {
 		i = (i + 1) % 256;
 		j = (j + s[i]) % 256;
 		x = s[i];
 		s[i] = s[j];
 		s[j] = x;
-		temp = s[(s[i] + s[j]) % 256];
-		vector.push(temp);
-		ciphertext.push(plaintext[y] ^ temp);
+		// temp = s[(s[i] + s[j]) % 256];
+		// vector.push(temp);
+		// ciphertext.push(plaintext[y] ^ temp);
+		// ciphertext.push(plaintext[y] ^ (s[(s[i] + s[j]) % 256]));
+		ciphertext.push(plaintext[y] ^ (s[(s[i] + s[j]) % 256]));
 	}
-	
+
 	return ciphertext;
 }
